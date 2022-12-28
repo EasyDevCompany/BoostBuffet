1d376ee27cf857f75b1f9dc81c52cf0eb9221bc5
Александр <tarasov.slavas2002@gmail.com>
2022-12-28 21:06:25 +0000

    добавил роутер для связки с апи для юзеров

--- app/app/api/api_v1/api.py
+++ app/app/api/api_v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
 from app.api.deps import create_session
-from app.api.api_v1.endpoints import webhook, posts
+from app.api.api_v1.endpoints import webhook, posts, telegram_user
 
 
 api_router = APIRouter()
 
 api_router.include_router(webhook.router, prefix="/webhook", tags=['webhook'])
 api_router.include_router(posts.router, prefix="/posts", tags=['posts'])
+api_router.include_router(telegram_user.router, prefix="/telegram_user", tags=['telegram_user'])
