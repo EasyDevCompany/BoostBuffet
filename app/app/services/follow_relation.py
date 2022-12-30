33cf9c6d76c3504a626c63512a8f9320bd01fdc5
Александр <tarasov.slavas2002@gmail.com>
2022-12-30 20:33:36 +0000

    добавил схему для эндпоинта на подписки

--- app/app/services/follow_relation.py
+++ app/app/services/follow_relation.py
@@ -2,6 +2,7 @@ from fastapi.responses import JSONResponse
 
 from app.repository.telegram_user import RepositoryTelegramUser, FollowRelationship
 
+from app.schemas.follow_relation import FollowIn
 
 class FollowService:
 
@@ -16,13 +17,13 @@ class FollowService:
     async def create_follow(
             self,
             follower_id: str,
-            following_id: str):
+            follow_in: FollowIn):
 
+        following_id = self._repository_telegram_user.get(telegram_id=follow_in.following_telegram_id).id
         if follower_id == following_id:
             return JSONResponse("Вы не можете подписаться на самого себя", status_code=400)
         if self._repository_follow_relation.get(FollowerID=follower_id, FollowingID=following_id):
             return JSONResponse("Вы уже подписаны на этого пользователя", status_code=400)
-
         return self._repository_follow_relation.create(
             obj_in={
                 "FollowerID": follower_id,
