944cac9cb9265866aadac6323bb3599e5068e87b
Александр <tarasov.slavas2002@gmail.com>
2022-12-30 20:29:30 +0000

    добавил схему в эндпоинт на подписку

--- app/app/api/api_v1/endpoints/telegram_user.py
+++ app/app/api/api_v1/endpoints/telegram_user.py
@@ -8,16 +8,18 @@ from app.core.containers import Container
 
 from app.api.deps import bot_token_verification
 
+from app.schemas.follow_relation import FollowIn
+
 
 router = APIRouter()
 
 @router.post("/subscribe")
 @inject
 async def subscribe(
-        user_id: str,
+        follow_in: FollowIn,
         token = Depends(bot_token_verification),
         follow_service = Depends(Provide[Container.follow_service])):
     return await follow_service.create_follow(
         follower_id=token,
-        following_id=user_id
+        follow_in=follow_in
     )
