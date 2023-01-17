f5e4a5f7676a9d388bb8b688b63cbae9a0758ade
Александр <tarasov.slavas2002@gmail.com>
2023-01-17 18:53:19 +0000

    Добавил пагинацию и логику для 3 тэга

--- app/app/services/cards.py
+++ app/app/services/cards.py
@@ -13,8 +13,8 @@ from app.models.cards import Cards
 
 from app.telegram_bot.loader import bot
 
-from app.schemas.cards import CardIn, UpdateCardIn
-
+from app.schemas.cards import CardIn, UpdateCardIn, TagIn
+from fastapi_pagination.ext.sqlalchemy import paginate
 
 class CardsService:
 
@@ -28,12 +28,9 @@ class CardsService:
 
     async def all_cards(
             self,
-            first_tag: Optional[str],
-            second_tag: Optional[str]):
-        represantation = {
-            "cards": self._repository_cards.all_cards(first_tag=first_tag, second_tag=second_tag)
-        }
-        return represantation
+            tag_in: TagIn):
+
+        return paginate(self._repository_cards.all_cards(tag_in=tag_in))
 
     async def create_card(self, user_id: str, card_in: CardIn):
         user = self._repository_telegram_user.get(id=user_id)
@@ -51,6 +48,7 @@ class CardsService:
                 "third_tag": card_in.third_tag,
                 "author": user,
                 "chat_open": card_in.chat_available,
+                "aprroval_status": "approved",
             },
             commit=True)
 
@@ -60,7 +58,10 @@ class CardsService:
             db_obj=card,
             obj_in={
                 "description": update_card_in.description or card.description,
-                "chat_open": update_card_in.chat_open or card.chat_open
+                "chat_open": update_card_in.chat_open or card.chat_open,
+                "first_tag": update_card_in.first_tag or card.first_tag,
+                "second_tag": update_card_in.second_tag or card.second_tag,
+                "third_tag": update_card_in.third_tag or card.third_tag
             }
         )
 
@@ -79,14 +80,14 @@ class CardsService:
 
         represantation = {"img_url": f"{settings.SERVER_IP}/image/{user.telegram_id}/{image.filename}"}
         return JSONResponse(status_code=200, content=represantation)
-# TODO Дописать логику если карты нет
+
     async def user_card(self, username):
         user = self._repository_telegram_user.get(username=username)
         return self._repository_cards.get(author_id=user.id)
 
     async def my_card(self, user_id):
         return self._repository_cards.get(author_id=user_id)
-# TODO Отправлять только юзерам у кого есть карточка
+
     async def send_message(self, username: str, text: str):
         user = self._repository_telegram_user.get(username=username)
         await bot.send_message(user.telegram_id, text=text)
